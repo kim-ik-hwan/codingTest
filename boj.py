f190eb8f3dac7f633060cdabc1b9bcc9a24a6145
Austin <kih.program@gmail.com>
2022-09-16 12:35:00 +0900

    BOJ_Step.1_Q1000

---
+++ boj.py
@@ -0,0 +1,16 @@
+# Q2557 (Hello World)
+
+print("Hello World!")
+
+
+# Q10718 (We love kriii)
+
+print("강한친구 대한육군")
+print("강한친구 대한육군")
+
+
+# Q1000 (A+B)
+
+A, B = input().split()
+print(int(A)+int(B))
+
