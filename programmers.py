021e1b7528a606e3a8034c560f1791d5091d9aed
Austin <kih.program@gmail.com>
2022-09-16 12:23:57 +0900

    Programmers_Lv.1_digitSum

--- programmers.py
+++ programmers.py
@@ -1,10 +1,19 @@
 # 짝수와 홀
 
-def solution(num):
-    if num%2==0:
-        return 'Even'
-    else:
-        return 'Odd'
+# def solution(num):
+#     if num%2==0:
+#         return 'Even'
+#     else:
+#         return 'Odd'
 
-print(solution(2))
-print(solution(3))
+# print(solution(2))
+# print(solution(3))
+
+
+# 자릿수 더하기
+
+def solution(n):
+    return sum(map(int, list(str(n))))
+
+print(solution(32))
+print(solution(153))
