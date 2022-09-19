b1b02e3fea87f83e14c47be061888267125b4ce1
Austin <kih.program@gmail.com>
2022-09-19 13:26:21 +0900

    Programmers_Lv.1_nextSqure

--- programmers.py
+++ programmers.py
@@ -41,7 +41,18 @@
 
 # 평균 구하기
 
-def solution(arr):
-    return (sum(arr) / len(arr))
-    
+# def solution(arr):
+#     return (sum(arr) / len(arr))
 
+
+# 정수 제곱근 판별
+
+def solution(n):
+    sqrt = n**(1/2)
+    
+    if sqrt % 1 == 0:
+        return (sqrt+1)**2
+    else: return -1
+
+print(solution(121))
+print(solution(11))
