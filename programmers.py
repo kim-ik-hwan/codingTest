f0a9525d76c1e31189dc464343fb0de7edcdfc0d
Austin <kih.program@gmail.com>
2022-10-05 11:04:24 +0900

    Programmers_Lv.1_rm_small

--- programmers.py
+++ programmers.py
@@ -302,4 +302,10 @@
 #     return sorted([n for n in arr if n%divisor == 0]) or [-1]
 
 
+# 제일 작은 수 제거하기
+
+# def solution(arr):
+#     arr.remove(min(arr))
+#     return arr if len(arr)!=0 else [-1]
+
 
