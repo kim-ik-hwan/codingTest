1af4e8cd07e2003348d0d7da56db62f070a6a164
Austin <kih.program@gmail.com>
2022-10-13 10:32:04 +0900

    Programmers_Lv.1_string_sort

--- programmers.py
+++ programmers.py
@@ -396,3 +396,13 @@
 #         s+=a[i]*b[i] 
 #     return s
 
+
+# 문자열 내림차순으로 배치하기
+
+# Ver.1 (choice)
+# def solution(s):
+#     return ''.join(sorted(s, reverse=True))
+
+# Ver.2
+# def solution(s):
+#     return (''.join(sorted(s)[::-1]))
